1101881316ed71328684b0b0335a3257080e7824
umesh47 <071bel2014@gmail.com>
2020-03-23 20:21:19 +0545

    add dynamic GET route

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get('/users',function(req,res){
     })
 })
 
-app.listen(8000, function(){
-    console.log("server is running")
+app.get('/users/:id', function(req, res){
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
 })
+
+//app.listen(8000, () => console.log("server is running")) alternative can be used as this too
+app.listen(8000, function(){console.log("server is running")})
